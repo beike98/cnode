1f9b4c5c6a0317557b2fb1d0012f6d3cad90c5ea
beike98 <49637524+beike98@users.noreply.github.com>
2021-08-07 16:43:42 +0800

    Navbar组件完成

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,25 @@
 <template>
   <div id="app">
+    <Nav></Nav>
   </div>
 </template>
+
+<script>
+import Nav from './components/Navbar.vue'
+export default {
+  components:{
+    Nav
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+a{
+  text-decoration: none;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="header">
+  <div class="nav">
+    <img src="../assets/cnodejs_light.svg" alt="logo">
+    <ul>
+      <li><a href="">首页</a></li>
+      <li><a href="">新手入门</a></li>
+      <li><a href="">API</a></li>
+      <li><a href="">关于</a></li>
+      <li><a href="">注册</a></li>
+      <li><a href="">登陆</a></li>
+    </ul>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Nav"
+}
+</script>
+
+<style scoped>
+.header{
+  height: 50px;
+  background: #444444;
+}
+.nav{
+  max-width: 1500px;
+  margin: 0 auto;
+}
+img{
+  width: 120px;
+  margin-left: 70px;
+  padding-top: 10px;
+}
+ul{
+  list-style:none;
+  float: right;
+  margin-right: 60px;
+  height: 50px;
+}
+ul>li{
+  display: inline-block;
+  margin-right: 30px;
+  font-size: 14px;
+  line-height: 50px;
+}
+a{
+  color: #cccccc;
+}
+a:hover{
+  color: #f1f1f1;
+}
+</style>
